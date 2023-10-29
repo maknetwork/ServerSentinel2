ec95bfad9724f3a46b888313c2b03388f40a9d1b
maknetwork <arif810khan@gmail.com>
2023-10-29 22:41:20 +0530

    done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const moment = require("moment"); // Added this line
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8080;
